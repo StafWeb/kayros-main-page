c21362a5f492bb9cb262488d6e879066e9b724e0
StafWeb <staf.target@yandex.ru>
2022-07-26 20:30:35 +0700

    fix

--- js/product.js
+++ js/product.js
@@ -1,5 +1,67 @@
 
 gsap.registerPlugin(ScrollTrigger);
+// let pageContainer = document.querySelector("#simple-scroll");
+// /* SMOOTH SCROLL */
+// const scroller = new LocomotiveScroll({
+//   el: pageContainer,
+//   smooth: true,
+//   getSpeed: 3,
+//   // reloadOnContextChange: true,
+//   getDirection: true,
+//   resetNativeScroll: true,
+// });
+// scroller.on("scroll", ScrollTrigger.update);
+// ScrollTrigger.scrollerProxy(pageContainer, {
+//   scrollTop(value) {
+//     return arguments.length
+//       ? scroller.scrollTo(value, 0, 0)
+//       : scroller.scroll.instance.scroll.y;
+//   },
+//   getBoundingClientRect() {
+//     return {
+//       left: 0,
+//       top: 0,
+//       width: window.innerWidth,
+//       height: window.innerHeight
+//     };
+//   },
+//   pinType: pageContainer.style.transform ? "transform" : "fixed"
+// });
+ScrollTrigger.defaults({
+  // toggleActions: "restart complete reverse reset",
+  toggleActions: "play none none none",
+  // markers: {
+  //   startColor: "green",
+  //   endColor: "red",
+  //   fontSize: "26px",
+  // },
+});
+let headAnim = gsap.timeline({ delay: 0.5});
+
+headAnim.from(".header__logo", { opacity: 0, duration: 0.6, ease: "power1.out" })
+  .from(".header__item-img", { opacity: 0, rotate: -90, duration: 0.4, ease: "power1.out" }, "-=0.3")
+  .from(".first-link", { opacity: 0, duration: 0.5, ease: "expo.out" }, "-=0.4")
+  .from(".second-link", { opacity: 0, x: -5, duration: 0.6, ease: "power1.out" }, "-=0.3")
+  .from(".header__form", { opacity: 0, x: -5, duration: 0.6, ease: "power1.out" }, "-=0.5")
+  .from(".header__user", { opacity: 0, x: -5, duration: 0.6, ease: "power1.out" }, "-=0.5")
+  .from(".header__cart", { opacity: 0, x: -5, duration: 0.6, ease: "power1.out" }, "-=0.5")
+  .from(".header__burger", { opacity: 0, x: -5, duration: 0.6, ease: "power1.out" }, "-=0.5");
+
+let footeTl = gsap.timeline({
+  ease: "power1.out",
+  scrollTrigger: {
+    trigger: ".footer",
+    start: "20% 85%",
+    toggleActions: "play none none none",
+    // endTrigger: ".footer",
+    // end: "bottom bottom",
+    scrub: false,
+  }
+});
+footeTl.from(".footer__contacts", { opacity: 0, y: 10, duration: 0.65 })
+  .from(".footer__nav", { opacity: 0, y: 10, duration: 0.65 }, "-=0.65")
+  .from(".footer__bottom-btn", { opacity: 0, y: 30, duration: 0.65 }, "+=0.8")
+  .from(".footer__bottom-descr", { opacity: 0, y: 30, duration: 0.65 }, "-=0.8");
 
 ScrollTrigger.refresh();
 let rootElement = document.documentElement;
@@ -53,9 +115,10 @@ navItems.forEach(el => {
   });
 });
 
-document.querySelectorAll(".simple-scroll").forEach(el => {
+document.querySelectorAll(".filters").forEach(el => {
   new SimpleBar(el);
 });
+// new SimpleBar(document.querySelector('#simple-scroll'));
 
 const footerBtn = document.querySelectorAll("[data-menu]");
 footerBtn.forEach(function (btn) {
@@ -104,6 +167,13 @@ filterClose.addEventListener('click', function () {
   body.classList.remove('stop-scroll');
   filter.classList.remove("filters_active");
   filterClose.classList.remove("burger_active");
+});
+
+const cardsBreadcumbsBtn = document.querySelectorAll(".cards__breadcrumbs-btn");
+cardsBreadcumbsBtn.forEach(el => {
+  el.addEventListener('click', () => {
+    el.remove('');
+  });
 });
 
 

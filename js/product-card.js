52349b563665a6349873b46bd91086752ca21f32
StafWeb <staf.target@yandex.ru>
2022-07-29 17:01:29 +0700

    fix

--- js/product-card.js
+++ js/product-card.js
@@ -1,14 +1,14 @@
 
 
 if (window.innerWidth > 580) {
-  const sliderThumbs = new Swiper('.slider__thumbs .swiper-container', { 
-    direction: 'vertical', 
-    slidesPerView: 'auto', 
+  const sliderThumbs = new Swiper('.slider__thumbs .swiper-container', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
     spaceBetween: 17,
     grabCursor: true,
     navigation: {
       nextEl: '.slider__next',
-      prevEl: '.slider__prev' 
+      prevEl: '.slider__prev'
     },
     breakpoints: {
       0: { // при 0px и выше
@@ -64,20 +64,22 @@ if (window.innerWidth < 580) {
   });
 };
 
-let imgZoomBtn = document.querySelectorAll(".slider__zoom .slider__image");
-let imgZoomWrapper = document.querySelector(".slider__zoom");
-let swapperWrapper = document.querySelector(".slider__zoom .swiper-container");
-const body = document.body;
-imgZoomBtn.forEach(el => {
-  el.addEventListener('click', () => {
-    body.classList.add('stop-scroll');
-    imgZoomWrapper.classList.add('slider__zoom_active');
+// if (window.innerWidth > 1000) {
+  let imgZoomBtn = document.querySelectorAll(".slider__zoom .slider__image");
+  let imgZoomWrapper = document.querySelector(".slider__zoom");
+  let swapperWrapper = document.querySelector(".slider__zoom .swiper-container");
+  const body = document.body;
+  imgZoomBtn.forEach(el => {
+    el.addEventListener('click', () => {
+      body.classList.add('stop-scroll');
+      imgZoomWrapper.classList.add('slider__zoom_active');
+    });
   });
-});
-swapperWrapper.addEventListener("click", function (evt) {
-  evt.stopPropagation();
-});
-imgZoomWrapper.addEventListener('click', () => {
-  body.classList.remove('stop-scroll');
-  imgZoomWrapper.classList.remove('slider__zoom_active');
-});
+  swapperWrapper.addEventListener("click", function (evt) {
+    evt.stopPropagation();
+  });
+  imgZoomWrapper.addEventListener('click', () => {
+    body.classList.remove('stop-scroll');
+    imgZoomWrapper.classList.remove('slider__zoom_active');
+  });
+// }

285a1c04c41a17646c936ad2c4519be6d011ee29
StafWeb <staf.target@yandex.ru>
2022-08-06 00:25:53 +0700

    add breakpoints

--- js/order-reg.js
+++ js/order-reg.js
@@ -1,21 +1,20 @@
 let orderForm = document.querySelector(".order-form");
-let orderFormBtn = orderForm.querySelector(".order-form__btn");
-let reqFields = orderForm.querySelectorAll(".required"); 
+let reqFields = orderForm.querySelectorAll(".required");
 
-orderForm.addEventListener('click', (event) => {
+orderForm.addEventListener('submit', (event) => {
   event.preventDefault();
 });
 reqFields.forEach(el => {
-  el.addEventListener('click', () => {
+  el.addEventListener('focus', () => {
     let elWrapper = el.parentNode;
     let fieldWrapper = elWrapper.parentNode;
-    el.value.length < 4 ?  (fieldWrapper.classList.add("required_active"), console.log("click1")) : (fieldWrapper.classList.remove("required_active"), console.log("click2"));
+    el.value.length < 4 ? (fieldWrapper.classList.add("required_active"), console.log("click1")) : (fieldWrapper.classList.remove("required_active"), console.log("click2"));
   })
 });
 reqFields.forEach(el => {
   el.addEventListener('focusout', () => {
     let elWrapper = el.parentNode;
     let fieldWrapper = elWrapper.parentNode;
-    el.value.length < 4 ?  (fieldWrapper.classList.add("required_active"), console.log("focusout1")) : (fieldWrapper.classList.remove("required_active"), console.log("focusout2"));
+    el.value.length < 4 ? (fieldWrapper.classList.add("required_active"), console.log("focusout1")) : (fieldWrapper.classList.remove("required_active"), console.log("focusout2"));
   })
 });

07679f45a1454bfb12661a89b091601ed0e5a36a
StafWeb <staf.target@yandex.ru>
2022-08-05 21:43:06 +0700

    fix

--- js/order-reg.js
+++ js/order-reg.js
@@ -1,22 +1,33 @@
 let orderForm = document.querySelector(".order-form");
 let orderFormBtn = orderForm.querySelector(".order-form__btn");
 let reqFields = orderForm.querySelectorAll(".required");
-let validCheck = () => {
-  for (let i = 0; i < reqFields.length; i++) {
-    let field = reqFields[i].value;
-    let fieldWrapper = reqFields[i].parentNode;
-    if (!field.length < 4) {
-      fieldWrapper.parentNode.classList.add("required_active")
-      orderFormBtn.disabled = true;
-    }
-    if (field.length >= 4) {
-      fieldWrapper.parentNode.classList.remove("required_active")
-      orderFormBtn.disabled = false;
-    }
-  }
-};
 
 orderForm.addEventListener('click', (event) => {
   event.preventDefault();
-  validCheck();
+});
+reqFields.forEach(el => {
+  el.addEventListener('click', () => {
+    let fieldWrapper = el.parentNode;
+    if (el.value.length <= 3){
+      console.log("1")
+      fieldWrapper.parentNode.classList.add("required_active")
+    }
+    if (el.value.length >= 4){
+      console.log("2")
+      fieldWrapper.parentNode.classList.remove("required_active")
+    }
+  })
+});
+reqFields.forEach(el => {
+  el.addEventListener('focusout', () => {
+    let fieldWrapper = el.parentNode;
+    if (el.value.length <= 3){
+      fieldWrapper.parentNode.classList.add("required_active")
+      console.log("1")
+    }
+    if (el.value.length >= 4){
+      fieldWrapper.parentNode.classList.remove("required_active")
+      console.log("2")
+    }
+  })
 });

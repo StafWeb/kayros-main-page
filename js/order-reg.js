52879af01848e302cca4b65c1730d8a402f9d844
StafWeb <staf.target@yandex.ru>
2022-08-06 01:37:00 +0700

    Update order-reg.js

--- js/order-reg.js
+++ js/order-reg.js
@@ -1,36 +1,28 @@
 let orderForm = document.querySelector(".order-form");
 let orderFormBtn = orderForm.querySelector(".order-form__btn");
 let reqFields = orderForm.querySelectorAll(".required");
-
+let chekForm = (elem) => {
+  let elWrapper = elem.parentNode;
+  let fieldWrapper = elWrapper.parentNode;
+  elem.value.length < 4 ? (fieldWrapper.classList.add("required_active"), console.log("focusout1")) : (fieldWrapper.classList.remove("required_active"), console.log("focusout2"));
+};
+let btnDis = () => {
+  for (let i = 0; i < reqFields.length; i++) {
+    reqFields[i].value.length < 4 ? orderFormBtn.disabled = true : orderFormBtn.disabled = false;
+  }
+};
 orderForm.addEventListener('submit', (event) => {
   event.preventDefault();
+  for (let i = 0; i < reqFields.length; i++) {
+    let elem = reqFields[i];
+    chekForm(elem);
+  }
+  btnDis();
 });
-// reqFields.forEach(el => {
-//   el.addEventListener('focus', () => {
-//     chekForm(el);
-//   })
-// });
 reqFields.forEach(el => {
   el.addEventListener('focusout', () => {
     chekForm(el);
     btnDis();
   })
 });
-orderFormBtn.addEventListener('click', () => {
-  for (let i = 0; i < reqFields.length; i++) {
-    let elem = reqFields[i];
-    chekForm(elem);
-  }
-  btnDis();
-})
-let chekForm = (elem) => {
-  let elWrapper = elem.parentNode;
-  let fieldWrapper = elWrapper.parentNode;
-  elem.value.length < 4 ? (fieldWrapper.classList.add("required_active"), console.log("focusout1")) : (fieldWrapper.classList.remove("required_active"), console.log("focusout2"));
-};
 
-let btnDis = () => {
-  for (let i = 0; i < reqFields.length; i++) {
-    reqFields[i].value.length < 4 ? orderFormBtn.disabled = true : orderFormBtn.disabled = false;
-  }
-};

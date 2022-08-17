f7fc2073a8902a45ca9bdb09d8cb0b5421925894
StafWeb <staf.target@yandex.ru>
2022-08-17 20:09:33 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -27,21 +27,39 @@
 //   };
 // });
 // });
-lightGallery(document.getElementById('animated-thumbnails-gallery'), {
-  // animateThumb: false,
-  // zoomFromOrigin: false,
-  // allowMediaOverlap: true,
-  licenseKey: "0000-0000-000-0000",
-  toggleThumb: true,
-  plugins: [lgZoom, lgThumbnail],
-  // mobileSettings: {
-  //   controls: false,
-  //   showCloseIcon: false,
-  //   download: false,
-  //   rotate: false
-  // }
+// lightGallery(document.getElementById('animated-thumbnails-gallery'), {
+//   // animateThumb: false,
+//   // zoomFromOrigin: false,
+//   // allowMediaOverlap: true,
+//   licenseKey: "0000-0000-000-0000",
+//   toggleThumb: true,
+//   plugins: [lgZoom, lgThumbnail],
+//   // mobileSettings: {
+//   //   controls: false,
+//   //   showCloseIcon: false,
+//   //   download: false,
+//   //   rotate: false
+//   // }
+// });
+$(document).ready(function() {
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		// image: {
+		// 	tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+		// 	titleSrc: function(item) {
+		// 		return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+		// 	}
+		// }
+	});
 });
-
 
 gsap.registerPlugin(ScrollTrigger);
 let pageContainer = document.querySelector("#viewport");

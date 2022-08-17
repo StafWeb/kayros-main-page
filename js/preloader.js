1b26524a5f93048422e89a12212d7f376fe78f0b
StafWeb <staf.target@yandex.ru>
2022-08-18 01:51:19 +0700

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -1,13 +1,13 @@
 import { headerAnim } from './main.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  let preloader = document.querySelector(".preloader");
-  let imgCount = document.querySelectorAll("img").length;
-  let prelInner = document.querySelector(".preloader__inner");
-  let counter = document.querySelector(".preloader__count");
-  let percent = 100 / imgCount;
-  let count = 0;
-  let imgload = 0;
+  let preloader = document.querySelector(".preloader"),
+      imgCount = document.querySelectorAll("img").length,
+      prelInner = document.querySelector(".preloader__inner"),
+      counter = document.querySelector(".preloader__count"),
+      percent = 100 / imgCount,
+      count = 0,
+      imgload = 0;
   for (let i = 0; i < imgCount; i++) {
     let imgCopy = new Image();
     imgCopy.src = document.images[i].src;
@@ -15,21 +15,17 @@ document.addEventListener('DOMContentLoaded', () => {
     imgCopy.onerror = prel;
   }
   function prel() {
-    let a = (count += percent).toFixed()
+    let a = (count += percent).toFixed();
     let c = imgload++;
     counter.textContent = a;
     prelInner.style.width = `${a}` + '%';
     window.onload = () => {
-      setTimeout(() => {
-        if (a >= 99 || c == imgCount) {
-          preloader.classList.add('preloader_hide');
-          if (window.innerWidth >= 1000) {
-            headerAnim();
-          }
+      if (a >= 99 || c == imgCount) {
+        preloader.classList.add('preloader_hide');
+        if (window.innerWidth >= 1000) {
+          headerAnim();
         }
-      }, 100);
-      
+      }
     }
-    
-  }
+  };
 });

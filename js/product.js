ab2810896d40c0dc56949104e0c05e70181933b5
StafWeb <staf.target@yandex.ru>
2022-08-31 19:46:03 +0700

    new anim

--- js/product.js
+++ js/product.js
@@ -11,23 +11,27 @@ ScrollTrigger.defaults({
   // },
 });
 if (window.innerWidth > 1000) {
-  let headAnim = gsap.timeline({ delay: 0.2, ease: "power1.out" });
-  headAnim.from(".header__logo", { opacity: 0, duration: 0.6 })
-    .from(".header__item-img", { opacity: 0, rotate: -90, duration: 0.4 }, "-=0.4")
+  let headAnim = gsap.timeline({ delay: 0.4, ease: "power1.out" });
+  headAnim.from(".breadcrumbs", { opacity: 0, duration: 0.6 })
+    .from(".product__title", { opacity: 0, duration: 0.6 }, "-=0.5")
+    .from(".cards__top", { opacity: 0, duration: 0.6 }, "-=0.6")
+    .from(".cards__list", { opacity: 0, y: 5, duration: 1 }, "-=0.6")
+    .from(".filters", { opacity: 0, duration: 1 }, "-=0.8")
+    .from(".header__logo", { opacity: 0, duration: 0.6 }, "-=0.9")
+    .from(".header__item-img", { opacity: 0, rotate: -90, duration: 0.4 }, "-=0.6")
     .from(".item1", { opacity: 0, duration: 0.5 }, "-=0.4")
     .from(".items", { opacity: 0, x: -5, duration: 0.6 }, "-=0.4")
     .from(".header__form", { opacity: 0, x: -5, duration: 0.6 }, "-=0.5")
     .from(".header__user", { opacity: 0, x: -5, duration: 0.6 }, "-=0.5")
     .from(".header__cart", { opacity: 0, x: -5, duration: 0.6 }, "-=0.5")
-    .from(".header__burger", { opacity: 0, x: -5, duration: 0.6 }, "-=0.5")
-    .fromTo(".cards", { opacity: 0, y: 20, }, { opacity: 1, y: 0, duration: 1 }, "-=0.5");
+    .from(".header__burger", { opacity: 0, x: -5, duration: 0.6 }, "-=0.5");
   let cardItem = document.querySelectorAll('.cards__item');
   for (let a = 0; a < cardItem.length; a++) {
     let trig = cardItem[a];
     gsap.from(trig, {
       opacity: 0,
       y: 20,
-      duration: 1.5,
+      duration: 0.6,
       ease: "power2.out",
       scrollTrigger: {
         trigger: trig,
@@ -46,7 +50,7 @@ if (window.innerWidth > 1000) {
   });
   footeTl.from(".footer__contacts", { opacity: 0, y: 10, duration: 0.65 })
     .from(".footer__nav", { opacity: 0, y: 10, duration: 0.65 }, "-=0.65")
-    .fromTo(".footer__bottom-btn", { y: 10, opacity: 0 }, { y:0, opacity:1, duration: 0.4 })
+    .fromTo(".footer__bottom-btn", { y: 10, opacity: 0 }, { y: 0, opacity: 1, duration: 0.4 })
     .from(".footer__bottom-descr", { opacity: 0, y: 10, duration: 0.4 });
 
   let infoTitles = document.querySelectorAll('.product__info > h2, p');
@@ -55,7 +59,7 @@ if (window.innerWidth > 1000) {
     gsap.from(trig, {
       opacity: 0,
       y: 20,
-      duration: 1.5,
+      duration: 0.6,
       ease: "power2.out",
       scrollTrigger: {
         trigger: trig,

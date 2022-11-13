a1e25a1fada21ae4738f3241066af75b91d29e3f
StafWeb <staf.target@yandex.ru>
2022-11-13 19:43:50 +0300

    Added new page

---
+++ js/order-history.js
@@ -0,0 +1,10 @@
+import { burgerScroll, windowResize } from './commponents/function.js'
+import { burger, footerMenu, burgerMenu, catalogLink } from './commponents/menu.js'
+
+burger();
+footerMenu();
+burgerMenu();
+catalogLink();
+windowResize();
+burgerScroll();
+

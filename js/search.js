8fde85db3ca66d06e15d7f31779390980a3f44f5
StafWeb <staf.target@yandex.ru>
2022-11-17 14:43:46 +0300

    Added new page

---
+++ js/search.js
@@ -0,0 +1,11 @@
+import { burgerScroll, windowResize, sortList } from './commponents/function.js'
+import { burger, footerMenu, burgerMenu, catalogLink } from './commponents/menu.js'
+
+
+sortList();
+burger();
+footerMenu();
+burgerMenu();
+catalogLink();
+windowResize();
+burgerScroll();
